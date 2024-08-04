89e0e8c905d5c318f7fdde5a3ce84601ac8fdaf7
Anil <anilsolanki1834@gmail.com>
2024-08-04 19:59:22 +0530

    Update Web_cam.py file

--- Web_Cam.py
+++ Web_Cam.py
@@ -8,7 +8,9 @@ if not cap.isOpened():
 while True:
     ret, frame = cap.read()
     frame = cv2.resize(frame, None, fx=1, fy=1, interpolation=cv2.INTER_AREA)
-    
+    cv2.imshow('Input', frame)
+    c = cv2.waitKey(1)
+    if c == 27:
+        break
 
-cap.release()
-cv2.destryAllWindow()
+
