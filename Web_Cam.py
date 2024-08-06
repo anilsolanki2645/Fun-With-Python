9c3625bc6a5e68009ddd2b8fc39a3c63afa4fb9e
Anil <anilsolanki1834@gmail.com>
2024-08-06 20:05:11 +0530

    Update Web_cam.py file

--- Web_Cam.py
+++ Web_Cam.py
@@ -1,15 +1,11 @@
 #open wecam using open cv2
 
 import cv2
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    raise IOError("cant open web cam")
 
 
-while True:
-    ret, frame = cap.read()
-    frame = cv2.resize(frame, None, fx=1, fy=1, interpolation=cv2.INTER_AREA)
-    cv2.imshow('Input', frame)
-    c = cv2.waitKey(1)
-    if c == 27:
-        break
 
 cap.release()
 cv2.destryAllWindow()
