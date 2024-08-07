dab87c3f71b11e44fc737ff93b6d57a8b1c2b6a5
Anil <anilsolanki1834@gmail.com>
2024-08-07 22:19:25 +0530

    Update Web_cam.py file

--- Web_Cam.py
+++ Web_Cam.py
@@ -10,7 +10,7 @@ while True:
     frame = cv2.resize(frame, None, fx=1, fy=1, interpolation=cv2.INTER_AREA)
     cv2.imshow('Input', frame)
     c = cv2.waitKey(1)
-    
+    if c == 27:
         break
 
 cap.release()
